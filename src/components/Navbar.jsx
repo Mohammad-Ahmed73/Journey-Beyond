ec9aad51a6ff1a7f0bee13c18c7d696d9c3d51f6
Ahmed <Naiyaab@nkdigitalsolutions.com>
2024-05-10 03:48:13 +0500

    Navbar set in mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,92 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import "../assets/css/navbar.css";
 
 function Header() {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return (
-        <>
-            <Navbar expand="lg">
-                <Container fluid="lg">
-                    <Navbar.Brand>
-                        <Link to="/" className='nav-link'>
-                            Home
-                        </Link>
-                    </Navbar.Brand>
-                    <div className='d-flex align-items-center nav-main-menu'>
-                        <Navbar.Toggle aria-controls="navbar-nav">
-                            <i class="fa-solid fa-bars open"></i>
-                            <i class="fa-solid fa-xmark close"></i>
-                        </Navbar.Toggle>
-                        <Button variant="primary" onClick={handleShow}>
-                            <i class="fa-solid fa-bars open"></i>
-                            <i class="fa-solid fa-xmark close"></i>
-                        </Button>
-                        <Navbar.Collapse id="navbar-nav">
-                            <Nav className="w-100 justify-content-lg-end">
-                                <Link to="/ecosystem" className='nav-link'>Ecosystem</Link>
-                                <Link to="/guided-services" className='nav-link'>Guided Services</Link>
-                                <Link to="/dashboard" className='nav-link'>Dashboard</Link>
-                                <Link to="/connect-wallet" className='nav-link'>Connect Wallet</Link>
-                                <Link to="/mint-now" className='btn nav-link'>Mint Now</Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </div>
-                </Container>
-            </Navbar>
-            <Offcanvas show={show} onHide={handleClose} className="mobileNavbar">
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <Link to="/ecosystem" className='nav-link'>Ecosystem</Link>
-                    <Link to="/guided-services" className='nav-link'>Guided Services</Link>
-                    <Link to="/dashboard" className='nav-link'>Dashboard</Link>
-                    <Link to="/connect-wallet" className='nav-link'>Connect Wallet</Link>
-                    <Link to="/mint-now" className='btn nav-link'>Mint Now</Link>
-                </Offcanvas.Body>
-            </Offcanvas>
-        </>
-    );
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <Navbar expand="lg">
+        <Container fluid="lg">
+          <Navbar.Brand className="d-none d-lg-block">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </Navbar.Brand>
+          <div className="d-flex align-items-center justify-content-between nav-main-menu w-100">
+            <Button className="navbar-toggler" onClick={handleShow}>
+              <i class="fa-solid fa-bars"></i>
+            </Button>
+            <Navbar.Collapse id="navbar-nav" className="d-none d-lg-flex">
+              <Nav className="w-100 justify-content-lg-end">
+                <Link to="/ecosystem" className="nav-link">
+                  Ecosystem
+                </Link>
+                <Link to="/guided-services" className="nav-link">
+                  Guided Services
+                </Link>
+                <Link to="/dashboard" className="nav-link">
+                  Dashboard
+                </Link>
+                <Link to="/connect-wallet" className="nav-link">
+                  Connect Wallet
+                </Link>
+                <Link to="/mint-now" className="btn nav-link">
+                  Mint Now
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+            <Link to="/mint-now" className="mobile-navlink-btn d-inline-block d-lg-none">
+                Connect Wallet
+            </Link>
+          </div>
+        </Container>
+        <Offcanvas show={show} onHide={handleClose} className="navbar-mobile">
+          <Offcanvas.Header closeButton>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <ul className="list-unstyled mb-0">
+              <li>
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/ecosystem" className="nav-link">
+                  Ecosystem
+                </Link>
+              </li>
+              <li>
+                <Link to="/guided-services" className="nav-link">
+                  Guided Services
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard" className="nav-link">
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link to="/connect-wallet" className="nav-link">
+                  Connect Wallet
+                </Link>
+              </li>
+              <li>
+                <Link to="/mint-now" className="btn nav-link">
+                  Mint Now
+                </Link>
+              </li>
+            </ul>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </Navbar>
+    </>
+  );
 }
 
 export default Header;
